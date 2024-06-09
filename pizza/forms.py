100b9a5dd70a716b954f44759936c4ae36830fd6
Akshay Patra <akshay.patra114@gmail.com>
2024-06-09 19:41:39 +0530

    used widgets in thw forms

--- pizza/forms.py
+++ pizza/forms.py
@@ -1,12 +1,5 @@
 from django import forms
-from .models import Pizza
-
-class PizzaForms(forms.ModelForm):
-    class Meta:
-        model=Pizza
-        fields=['topping1','topping2','size']
-        labels={'topping1':'Topping 1','topping2':'Topping 2','size':'Size'}
-
+from .models import Pizza,Size
 
 
 # class PizzaForms(forms.Form):
@@ -14,3 +7,13 @@ class PizzaForms(forms.ModelForm):
 #     topping1=forms.CharField(label='Topping1',max_length=100)
 #     topping2=forms.CharField(label='Topping2',max_length=100)
 #     size=forms.ChoiceField(label='Size',choices=[('Small','Small'),('Medium','Medium'),('Large','Large')])
+
+class PizzaForms(forms.ModelForm):
+
+    size=forms.ModelChoiceField(queryset=Size.objects,empty_label=None,widget=forms.RadioSelect)
+
+    class Meta:
+        model=Pizza
+        fields=['topping1','topping2','size']
+        labels={'topping1':'Topping 1','topping2':'Topping 2','size':'Size'}
+        
